8ceb7083ee17c674f002e5bb95ad05ec66221704
EC2 Default User <ec2-user@ip-172-31-29-117.us-west-1.compute.internal>
2017-12-02 16:12:52 +0000

    server

---
+++ analyzerPipeline/run.sh
@@ -0,0 +1,3 @@
+python /home/ec2-user/NewRepositoryOct2017/analyzerPipeline/analyzer.py 'Buffalo Wild Wings' 6 20001
+python /home/ec2-user/NewRepositoryOct2017/analyzerPipeline/analyzer.py Chipotle 7 20001
+python /home/ec2-user/NewRepositoryOct2017/analyzerPipeline/analyzer.py 'Red Robin' 8 20001
